a924fa804c28ba2a7fbc2a1871dfc0c5b78a2249
Prashant <prashantriyer@hotmail.com>
2020-11-19 16:32:15 +0800

    created files

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+import os
+
+client = discord.Client()
+
+
+# @client.event
+# async def on_member_join(member):
+#     await member.create_dm()
+#     await member.dm_channel.send(f'Hi {member.name}, in this server, I take executive decisions')
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name='you L'))
+
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    if message.author == 'azraxel5':
+        await message.channel.send('<@722427790871363584 L')
+
+client.run(os.environ['TOKEN'])
